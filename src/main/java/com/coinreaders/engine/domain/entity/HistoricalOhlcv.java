0b26f6ed351647790283630db9c5d2ee30c1dde1
KIYEONG <rldud1237@naver.com>
2025-10-27 16:57:02 +0900

    feat: HistoricalOhlcv 엔티티 생성을 위한 메서드 추가

--- src/main/java/com/coinreaders/engine/domain/entity/HistoricalOhlcv.java
+++ src/main/java/com/coinreaders/engine/domain/entity/HistoricalOhlcv.java
@@ -40,4 +40,19 @@ public class HistoricalOhlcv {
 
     @Column(nullable = false, precision = 30, scale = 8)
     private BigDecimal candleAccTradeVolume;
+
+    private HistoricalOhlcv(String market, LocalDateTime candleDateTimeKst, BigDecimal openingPrice, BigDecimal highPrice, BigDecimal lowPrice, BigDecimal tradePrice, BigDecimal candleAccTradeVolume) {
+        this.market = market;
+        this.candleDateTimeKst = candleDateTimeKst;
+        this.openingPrice = openingPrice;
+        this.highPrice = highPrice;
+        this.lowPrice = lowPrice;
+        this.tradePrice = tradePrice;
+        this.candleAccTradeVolume = candleAccTradeVolume;
+    }
+
+    // 엔티티 생성을 위한 public 정적 팩토리 메서드 추가
+    public static HistoricalOhlcv of(String market, LocalDateTime candleDateTimeKst, BigDecimal openingPrice, BigDecimal highPrice, BigDecimal lowPrice, BigDecimal tradePrice, BigDecimal candleAccTradeVolume) {
+        return new HistoricalOhlcv(market, candleDateTimeKst, openingPrice, highPrice, lowPrice, tradePrice, candleAccTradeVolume);
+    }
 }
